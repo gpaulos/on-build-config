749a71009918e675acce0c6b2e9fc6627dd237b1
paulog <george.paulos@emc.com>
2017-05-24 19:03:34 +0000

    Update

--- test.sh
+++ test.sh
@@ -1,5 +1,6 @@
 #!/bin/bash -ex
 export VCOMPUTE=("${NODE_NAME}-Rinjin1","${NODE_NAME}-Rinjin2","${NODE_NAME}-Quanta")
+export UCSPE=("${NODE_NAME}-UCSPE")
 RUN_FIT_TEST="${RUN_FIT_TEST}"
 if [ ! -z "${4}" ]; then
   RUN_FIT_TEST=$4
@@ -30,7 +31,7 @@ apiPackageModify() {
     bash make-deb.sh
     popd
     for package in ${API_PACKAGE_LIST}; do
-      sudo pip uninstall -y ${package//./-}
+      sudo pip uninstall -y ${package//./-} || true
       pushd ${WORKSPACE}/build-deps/on-http/$package
         fail=true
         while $fail; do
@@ -92,6 +93,16 @@ execWithTimeout() {
   fi
   set -e
 }
+
+ucsReset() {
+  cd ${WORKSPACE}/build-config/deployment/
+  if [ "${USE_VCOMPUTE}" != "false" ]; then
+    for i in ${UCSPE[@]}; do
+      ./vm_control.sh "${ESXI_HOST},${ESXI_USER},${ESXI_PASS},reset,1,${i}_*"
+    done
+  fi
+}
+
 nodesOff() {
   cd ${WORKSPACE}/build-config/deployment/
   if [ "${USE_VCOMPUTE}" != "false" ]; then
@@ -119,6 +130,9 @@ nodesOn() {
 nodesDelete() {
   cd ${WORKSPACE}/build-config/deployment/
   if [ "${USE_VCOMPUTE}" != "false" ]; then
+    if [ ${OVA_POST_TEST} == "true" ]; then
+      VCOMPUTE+=("${NODE_NAME}-ova-for-post-test")
+    fi
     for i in ${VCOMPUTE[@]}; do
       ./vm_control.sh "${ESXI_HOST},${ESXI_USER},${ESXI_PASS},delete,1,${i}_*"
     done
@@ -199,6 +213,12 @@ generateSolLog(){
         bash generate-sol-log.sh' > ${WORKSPACE}/sol.log &
 }
 
+generateSysLog(){
+  cd ${WORKSPACE}/RackHD/example
+  vagrant ssh -c 'dmesg > /home/vagrant/src/dmesg.log'
+  vagrant ssh -c 'cp /var/log/syslog /home/vagrant/src/syslog.log'
+}
+
 setupVirtualEnv(){
   pushd ${WORKSPACE}/RackHD/test
   rm -rf .venv/on-build-config
@@ -221,9 +241,9 @@ runTests() {
          echo "Test FIT failed running deploy/rackhd_stack_init.py"
          exit 1
      fi
-     python run_tests.py ${TEST_STRING} ${TEST_NODEINDEX} -stack vagrant -port 9090 -v 4 -xunit
+     python run_tests.py ${TEST_GROUP} -stack vagrant -port 9090 -v 4 -xunit
      if [ $? -ne 0 ]; then
-         echo "Test FIT failed running " + ${TEST_STRING} + "-" + ${TEST_GROUP}
+         echo "Test FIT failed running smoke test"
          exit 1
      fi
      mkdir -p ${WORKSPACE}/xunit-reports
@@ -266,34 +286,139 @@ waitForAPI() {
   fi
 }
 
+######################################
+#    OVA POST SMOKE TEST RELATED     #
+######################################
+portForwarding(){
+    # forward ova to localhost
+    # according to vagrant/mongo/config.json and cit/fit config
+    socat TCP4-LISTEN:9091,forever,reuseaddr,fork TCP4:$1:5672 &
+    socat TCP4-LISTEN:9090,forever,reuseaddr,fork TCP4:$1:8080 &
+    socat TCP4-LISTEN:9092,forever,reuseaddr,fork TCP4:$1:9080 &
+    socat TCP4-LISTEN:9093,forever,reuseaddr,fork TCP4:$1:8443 &
+    socat TCP4-LISTEN:2222,forever,reuseaddr,fork TCP4:$1:22 &
+    echo "Finished ova -> localhost port forwarding"
+    echo "5672->9091"
+    echo "8080->9090"
+    echo "9080->9092"
+    echo "8443->9093"
+    echo "22->2222"
+}
+
+fetchOVALog(){
+    ansible_workspace=${WORKSPACE}/build-config/jobs/build_ova/ansible
+    # fetch rackhd log
+    pushd $ansible_workspace
+      echo "ova-post-test ansible_host=$OVA_INTERNAL_IP ansible_user=$OVA_USER ansible_ssh_pass=$OVA_PASSWORD ansible_become_pass=$OVA_PASSWORD" > hosts
+      ansible-playbook -i hosts main.yml --tags "after-test"
+      mkdir -p ${WORKSPACE}/build-deps
+      for log in `ls *.log | xargs` ; do
+        cp $log ${WORKSPACE}/build-deps
+      done
+    popd
+}
+
+######################################
+#  OVA POST SMOKE TEST RELATED END   #
+######################################
+
 if [ "$RUN_CIT_TEST" == true ] || [ "$RUN_FIT_TEST" == true ] ; then
+  if [ "$TEST_TYPE" == "ova" ]; then
+    # based on the assumption that in the same folder, the VMs has been exist normally. so don't destroy VM here.
+    
+    nodesCreate
+    
+    # Prepare RackHD
+    # Forward local host port to ova
+    portForwarding ${OVA_INTERNAL_IP}
 
-  # register the signal handler to clean up( vagrantDestroy ), with process being killed
-  trap cleanupVMs SIGINT SIGTERM SIGKILL
-  cleanupVMs
+    # We setup the virtual-environment here, since once we
+    # call "nodesOn", it's a race to get to the first test
+    # before the nodes get booted far enough to start being
+    # seen by RackHD. Logically, it would be better IN runTests.
+    # We do it between the vagrant and waitForAPI to use the
+    # time to make the env instead of doing sleeps...
+    setupVirtualEnv
+    waitForAPI
+    nodesOn &
+    # Doesn't support ova smoke test now
+    # generateSolLog
+    # Run tests
+    runTests
+    # exit venv
+    deactivate
 
-  # based on the assumption that in the same folder, the VMs has been exist normally. so don't destroy VM here.
-  
-  nodesCreate
-  # Power on vagrant box and nodes 
-  vagrantUp
-  # We setup the virtual-environment here, since once we
-  # call "nodesOn", it's a race to get to the first test
-  # before the nodes get booted far enough to start being
-  # seen by RackHD. Logically, it would be better IN runTests.
-  # We do it between the vagrant and waitForAPI to use the
-  # time to make the env instead of doing sleeps...
-  setupVirtualEnv
-  waitForAPI
-  nodesOn &
-  generateSolLog
-  # Run tests
-  runTests
+    # Remedial work
+    # Specific remedial work
+    fetchOVALog
 
-  # Clean Up below
+    # Clean Up below
 
-  #shutdown vagrant box and delete all resource (like removing vm disk files in "~/VirtualBox VMs/")
-  cleanupVMs
-  nodesDelete
+    #shutdown vagrant box and delete all resource (like removing vm disk files in "~/VirtualBox VMs/")
+    cleanupVMs
+    nodesDelete
+  elif [ "$TEST_TYPE" == "docker" ]; then
+    # based on the assumption that in the same folder, the VMs has been exist normally. so don't destroy VM here.
+    
+    nodesCreate
+    
+    # Prepare RackHD
+    # Forward local host port to ova
+    portForwarding localhost
+
+    # We setup the virtual-environment here, since once we
+    # call "nodesOn", it's a race to get to the first test
+    # before the nodes get booted far enough to start being
+    # seen by RackHD. Logically, it would be better IN runTests.
+    # We do it between the vagrant and waitForAPI to use the
+    # time to make the env instead of doing sleeps...
+    setupVirtualEnv
+    waitForAPI
+    nodesOn &
+    # Doesn't support ova smoke test now
+    # generateSolLog
+    # Run tests
+    runTests
+    # exit venv
+    deactivate
+
+    # Clean Up below
+
+    #shutdown vagrant box and delete all resource (like removing vm disk files in "~/VirtualBox VMs/")
+    cleanupVMs
+    nodesDelete
+  else
+    # rese the UCSPE emulators 
+    ucsReset
+
+    # register the signal handler to clean up( vagrantDestroy ), with process being killed
+    trap cleanupVMs SIGINT SIGTERM SIGKILL
+    cleanupVMs
+
+    # based on the assumption that in the same folder, the VMs has been exist normally. so don't destroy VM here.
+    
+    nodesCreate
+    # Power on vagrant box and nodes 
+    vagrantUp
+    # We setup the virtual-environment here, since once we
+    # call "nodesOn", it's a race to get to the first test
+    # before the nodes get booted far enough to start being
+    # seen by RackHD. Logically, it would be better IN runTests.
+    # We do it between the vagrant and waitForAPI to use the
+    # time to make the env instead of doing sleeps...
+    setupVirtualEnv
+    waitForAPI
+    nodesOn &
+    generateSolLog
+    # Run tests
+    runTests
+    generateSysLog
+
+    # Clean Up below
+
+    #shutdown vagrant box and delete all resource (like removing vm disk files in "~/VirtualBox VMs/")
+    cleanupVMs
+    nodesDelete
+  fi
 
 fi
